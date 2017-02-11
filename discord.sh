7e7c0f835f22040c18dcc25c5919dd36b85992f9
toiry921 <toiry921@gmail.com>
2017-02-11 09:48:39 -0800

    Update discord.sh
    Updated to V0.0.1

--- discord.sh
+++ discord.sh
@@ -1,22 +1,13 @@
-#mkdir discord
+#Make a clean Directory and enter it
+rm -r discord
+mkdir discord
 cd discord
 
-#Fetch Requirements with Pacman
-sudo pacman -S wget npm nodejs git
+#Guessing you have Wget so leaving this commented out
+# sudo pacman -S wget
 
 # Install discord in /opt/
-#wget Get Discord build and place it in same dir as this script
-tar -xzvf discord-canary-0.0.8.tar.gz
+wget https://discordapp.com/api/download?platform=linux&format=tar.gz
+tar -xzvf discord-0.0.1.tar.gz
 sudo cp -r DiscordCanary /opt/
 sudo ln -s /opt/DiscordCanary/DiscordCanary /usr/bin/discord
-
-# Install BetterDiscord
-# Edit this out if you don't want it
-git clone https://github.com/sarnex/BetterDiscordApp.git
-mv BetterDiscordApp BetterDiscord
-cp BetterDiscord/Installers/Node/* .
-cp BetterDiscord/splice .
-
-#npm install asar wrench
-sudo ./install.sh
-
